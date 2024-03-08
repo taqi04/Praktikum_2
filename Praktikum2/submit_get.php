b253491eeb560d22a73582a6b8ba9b96ff600a65
Taqi.rabbani <taqirabbani56@gmail.com>
2024-03-08 10:37:19 +0700

    praktikum 3 - skills

--- Praktikum2/submit_get.php
+++ Praktikum2/submit_get.php
@@ -4,8 +4,8 @@ $Username = $_GET['username'];
 $password = $_GET['Password'];
 
 echo "<h1>Selamat {$namaLengkap}. Kamu sudah daftar</h1>";
-echo "<h3>Username {$Username}.</h3>";
-echo "<h3>Password {$password}.</h3>";
+echo "<h3>Username : {$Username}.</h3>";
+echo "<h3>Password : {$password}.</h3>";
 
 
 ?>
---
+++ Praktikum3/proses_registrasi.php
@@ -0,0 +1,12 @@
+<?php 
+//Array Numerik - Index & Value
+$domisili = ["Jakarta","Bogor","Depok","Tanggerang","Bekasi"];
+// Array asosiatif - Key & value
+$program_studi = [
+    "SI" => "Sistem Informasi",
+    "TI" => "Teknik Informatika",
+    "BD" => "Bisnis Digital",
+];    
+    
+
+?>
